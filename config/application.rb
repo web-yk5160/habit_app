6f950b56ed4c8b315babf52a8a188649ea47702e
yuta kimura <rubystudy0123@gmail.com>
2020-10-02 15:21:33 +0900

    modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module HabitApp
     # i18n
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
